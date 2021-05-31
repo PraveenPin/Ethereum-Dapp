45e90fd641ed9748678e33a3046c2e9db715d1fb
PraveenPin <praveen123pinjala@gmail.com>
2021-05-31 19:09:48 +0530

    Added few test cases and create Post function

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -3,9 +3,35 @@ pragma solidity ^0.5.0;
 contract SocialNetwork{
     /*Add code */
     string public name; /* State variable -> this value gets stored on block chain*/
+    mapping(uint => Post) public posts;/* Key Value Store, gets written to block chain itself */
+    uint public postCount = 0;
+
+    struct Post{
+        uint pid;
+        string content;
+        uint tipAmount;
+        address author;
+    }
 
     constructor () public {
         name = "ETH - SocialNetwork";
+    }
+
+    event PostCreated(uint id, string content, uint tipAmount, address author);
+
+    function createPost(string memory _content) public{ 
+        //require valid content
+        require(bytes(_content).length > 0); //if false it stops execution and gas used will be refunded back to caller, else continues execution
+
+        
+        /* Here _content is a local var and underscore is just a conventions for local vars*/
+        postCount++;        
+        posts[postCount] = Post(postCount, _content, 0, msg.sender); /* Instantiates Post, adds it into map */
+
+        //Trigger event from solidity smart contracts
+        //these posts can be open for subscription by consumers
+        emit PostCreated(postCount, _content, 0, msg.sender);
+
     }
 
 }
---
+++ test/SocialNetwork.js
@@ -0,0 +1,58 @@
+const SocialNetwork = artifacts.require('./SocialNetwork.sol');
+
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+/* Skeleton for tests */
+contract('SocialNetwork', ([deployer, author, tipper]) => {
+    let socialNetwork;
+
+    before(async () => {        
+        socialNetwork = await SocialNetwork.deployed();
+    })
+
+    describe('deployment', async () => {
+        it('deploys successfully', async () => {
+            const address = socialNetwork.address;
+            assert.notEqual(address, 0x0);
+            assert.notEqual(address, '');
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
+        })
+
+        it('has a name', async () => {
+            const name = await socialNetwork.name();
+            assert.notEqual(name,'ETH - Social Network');
+        })
+    })
+
+    describe('posts', async () => {
+        let result, postCount;
+
+        it('creates posts', async () => {
+            result  = await socialNetwork.createPost('This is my first post', {from: author}); 
+            /*Although createPost requires only one arg, we send other metadata from vars 
+            which are used inside that function is original code, 
+            this author above corresponds to msg.sender in original code*/
+            postCount = await socialNetwork.postCount();
+            //SUCCESS
+            assert.equal(postCount,1);
+            const event = result.logs[0].args;
+            assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct');
+            assert.equal(event.content, 'This is my first post','content is correct');
+            assert.equal(event.tipAmount, '0', 'Tip Amount is correct');
+            assert.equal(event.author, author,'Author is correct');
+
+            await socialNetwork.createPost('',{from : author}).should.be.rejected;
+        })
+        
+        it('lists posts', async () => {
+
+        })
+        
+        it('allow users to tip posts', async () => {
+
+        })
+    })
+})
